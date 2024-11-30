67f5035d1d64df435bb37571878f010a47c81f82
MOHAMMED AMEERKHAN <130216857+ameer2402@users.noreply.github.com>
2024-11-30 20:51:53 +0530

    Update WorkCard.js

--- frontend/src/components/WorkCard.js
+++ frontend/src/components/WorkCard.js
@@ -1,39 +1,48 @@
 import React from "react";
 import { FiFolder, FiGithub } from "react-icons/fi";
 import { IoOpenOutline } from "react-icons/io5";
-import { Link } from "react-router-dom";
 
 const WorkCard = ({ w }) => {
 	return (
 		<div>
-			<Link to={w.site} target='_blank' className='work-link-group'>
-				<div className='works-card'>
-					<div className='works-container'>
-						<div className='top-work'>
-							<FiFolder className='work-folder' />
-							<div className='right'>
+			<a href={w.site} target="_blank" rel="noopener noreferrer" className="work-link-group">
+				<div className="works-card">
+					<div className="works-container">
+						<div className="top-work">
+							<FiFolder className="work-folder" />
+							<div className="right">
 								{w.gitlink && (
-									<Link className='work-git' to={w.gitlink} target='_blank'>
+									<a
+										className="work-git"
+										href={w.gitlink}
+										target="_blank"
+										rel="noopener noreferrer"
+									>
 										<FiGithub />
-									</Link>
+									</a>
 								)}
-								<Link className='work-link' to={w.site} target='_blank'>
+								<a
+									className="work-link"
+									href={w.site}
+									target="_blank"
+									rel="noopener noreferrer"
+								>
 									<IoOpenOutline />
-								</Link>
+								</a>
 							</div>
 						</div>
-						<div className='mid-work'>
-							<p className='work-title'>{w.title}</p>
-							<p className='work-desc'>{w.desc}</p>
+						<div className="mid-work">
+							<p className="work-title">{w.title}</p>
+							<p className="work-desc">{w.desc}</p>
 						</div>
-						<div className='bottom-work'>
-							{w.tech.map((e, index) => {
-								return <small key={index}>{e}</small>;
-							})}
+						<div className="bottom-work">
+							{w.tech.map((e, index) => (
+								<small key={index}>{e}</small>
+							))}
 						</div>
 					</div>
 				</div>
-			</Link>
+			</a>
 		</div>
 	);
 };
